94fcc6eb18cb3c69b3340313873e9ebc6691482f
Guilherme Santos <guilhermesantos.developer@gmail.com>
2023-07-12 11:13:58 -0300

    layout da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logoImg from './assets/monge.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <img src={logoImg} alt="monge"  className='logo'/>
+      <h2 className='title'>Categorias</h2>
+      <section className='category-area'>
+        <button className='category-button'>Motivação</button>
+        <button className='category-button'>Bom dia</button>
+      </section>
+
+      <button className='button-frase'>Gerar Frase</button>
+
+      <p className='texto-frase'>Alguma frase qualquer</p>
+    </div> 
   )
 }
 
